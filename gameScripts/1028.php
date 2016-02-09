4650a99054759428db6ca218b7159c5dffe28045
rwruss <rwruss81@gmail.com>
2016-02-09 16:28:29 -0600

    2016-02-09W

--- gameScripts/1028.php
+++ gameScripts/1028.php
@@ -3,9 +3,10 @@
 // Get date for this unit
 $unitFile = fopen($gamePath./'unitDat.dat', 'rb');
 fseek($unitFile, $unitID*400);
-$groupDat = unpack('i*', fread($unitFile, 400));
-fclose($unitFile);
+$unitDat = unpack('i*', fread($unitFile, 400));
+
 
 // Give unit options and stats dependant upon unit type
-
+include('../gameScripts/units/wbsc_'.$unitDat[4].'.php');
+fclose($unitFile);
 ?>
