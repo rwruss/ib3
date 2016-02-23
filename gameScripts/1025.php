ba6c51572ba42a8351906a6b45f1a49258338421
rwruss <rwruss81@gmail.com>
2016-02-23 16:26:22 -0600

    2016-02-23W

--- gameScripts/1025.php
+++ gameScripts/1025.php
@@ -1,6 +1,6 @@
 <?php
 
-include('./taskFunctions.php');
+//include('./taskFunctions.php');
 include("./slotFunctions.php");
 
 $unitList = array_slice($postVals, 1);
@@ -9,31 +9,58 @@ $unitList = array_slice($postVals, 1);
 $unitFile = fopen($gamePath.'/unitDat.dat','rb');
 $processOrder = false;
 
-
-
+$forageLocs = [0,0,0];
 foreach ($unitList as $unitID) {
 	fseek($unitFile, $unitID*$defaultBlockSize);
 	$unitDat = unpack('i*', fread($unitFile, $unitBlockSize));
 	echo 'Owner: '.$unitDat[5].', Controller '.$unitDat[6].'<br>';
+	
+	if ($forageLocs[0] == 0) {
+		$forageLocs[0] = $unitDat[1]; // X coordinate
+		$forageLocs[1] = $unitDat[2]; // Y coordinate
+		$forageLocs[2] = 11; // diameter
+	} else {
+		if ($forageLocs[0] != $unitDat[1] || $forageLocs[1] != $unitDat[2]) {
+			exit('Invalid locations');
+		}
+	}
 	if ($unitDat[5] == $pGameID) {
 		echo 'Accept Order';
 		$processOrder = true;
 		$orderList[] = $unitID;
+	} else {
+		$processOrder = false;
+		break;
 	}
 }
 
 if ($processOrder) {
+	$rowSize = 14400; // (120 degrees x 120 points)
+	$leftEdge = $forageLocs[1] - floor($forageLocs[2]/2);
 	echo 'Final list of units to process:';
 	print_r($orderList);
-
+	$slotFile = fopen($gamePath.'/gameSlots.slt', 'r+b');
+	
+	// Load the map and resource data for the affected area
+	$terrainDat = "";
+	$terrainFile = fopen();
+	for ($row = 0; $row<$forageLocs[2]; $row++) {
+		fseek($terrainFile, ($forageLocs[1]+$row)*$rowSize+$leftEdge);
+		$terrainDat .= fread($terrainFile, $forageLocs[2]);
+	}
+	
+	fclose($terrainFile);
+	$terrainInfo = unpack("C*", $terrainDat);
+	/*
+	
 	// Create a new task to be processed.
 	$taskFile = fopen($gamePath.'/tasks.tdt', 'r+b');
 	$taskIndex = fopen($gamePath.'/tasks.tix', 'r+b');
-	$slotFile = fopen($gamePath.'/gameSlots.slt', 'r+b');
+	
 	$newTask = createTask($taskFile, $taskIndex, 24*60, 0,$gamePath, $slotFile); //createTask($taskFile, $taskIndex, $duration, $parameters, $gamePath, $slotFile)
 	fclose($taskFile);
 	fclose($taskIndex);
-
+	
 
 	// get player Dat for slots
 	$playerFile = fopen($gamePath.'/players.plr', 'r+b');
@@ -47,6 +74,7 @@ if ($processOrder) {
 		$playerDat[29] = $newSlot;
 	}
 	fclose($playerFile);
+	
 	// Record new task in player's ongoing task list
 	addDataToSlot($gamePath."/gameSlots.slt", $playerDat[29], pack("N", $newTask), $slotFile);
 
@@ -56,6 +84,7 @@ if ($processOrder) {
 		fseek($unitFile, $unitID*$defaultBlockSize+11*4);
 		fwrite($unitFile, $taskNumBin);
 	}
+	*/
 	fclose($slotFile);
 }
 
