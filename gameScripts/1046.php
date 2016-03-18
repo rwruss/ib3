390e8c47e705b80a460dc92529e6f6bf9148a889
rwruss <rwruss81@gmail.com>
2016-03-18 11:30:28 -0500

    2016-03-18Wa

--- gameScripts/1046.php
+++ gameScripts/1046.php
@@ -9,31 +9,14 @@ $unitDat = unpack('i*', fread($unitFile, $unitBlockSize));
 $actionPoints = min(1000, $unitDat[16] + floor((time()-$unitDat[27])/$unitDat[17]));	
 
 if ($actionPoints > 20) {
+	
 	// Load Task Data
 	$taskFile = fopen($gamePath.'/tasks.tdt', 'r+b');
 	fseek($taskFile, $taskID*100);
 	$taskDat = unpack('i*', fread($taskFile, 100));
 
-	// If task is not complete, add action point and subtract from the unit
-	if ($taskDat[6] < $taskDat[5])  {
-		$spentPoints = max($taskDat[5] - $taskDat[6], 20);
-		
-		$actionPoints -= $spentPoints;
-		$taskDat[6] += $spentPoints;
-		
-		fseek($taskFile, $taskID*100+20);
-		fwrite($taskFile, pack('i', $taskDat[6]));
-		
-		fseek($unitFile, $postVals[1]*$defaultBlockSize+60);
-		fwrite($unitFile, pack('i', $actionPoints));
-		
-		fseek($unitFile, $postVals[1]*$defaultBlockSize+104);
-		fwrite($unitFile, pack('i', time()));
-		
-		if ($taskDat[6] >= $taskDat[5]) { // process completion of task
-		
-		}		
-	}
+	include('./tasks/ta_'.$taskDat[7].'.php');
+	
 
 	// If task is complete, make no changes
 
---
+++ gameScripts/tasks/ta_2.php
@@ -0,0 +1,24 @@
+<?php
+
+// If task is not complete, add action point and subtract from the unit
+	if ($taskDat[6] < $taskDat[5])  {
+		$spentPoints = max($taskDat[5] - $taskDat[6], 20);
+		
+		$actionPoints -= $spentPoints;
+		$taskDat[6] += $spentPoints;
+		
+		fseek($taskFile, $taskID*100+20);
+		fwrite($taskFile, pack('i', $taskDat[6]));
+		
+		fseek($unitFile, $postVals[1]*$defaultBlockSize+60);
+		fwrite($unitFile, pack('i', $actionPoints));
+		
+		fseek($unitFile, $postVals[1]*$defaultBlockSize+104);
+		fwrite($unitFile, pack('i', time()));
+		
+		if ($taskDat[6] >= $taskDat[5]) { // process completion of task
+		
+		}		
+	}
+
+?>
