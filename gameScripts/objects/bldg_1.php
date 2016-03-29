09bdc3ed931d10b21d8449d5d390bc080c086c59
rwruss <rwruss81@gmail.com>
2016-03-29 16:39:38 -0500

    2016-03-29

--- gameScripts/objects/bldg_1.php
+++ gameScripts/objects/bldg_1.php
@@ -13,7 +13,17 @@ echo '<script>
   newTabMenu("bldg_'.$postVals[1].'");
   newTab("bldg_'.$postVals[1].'", 1, "Building Info");
   newTab("bldg_'.$postVals[1].'", 2, "Building  Tasks");
+  newTab("bldg_'.$postVals[1].'", 3, "Building  Upgrades")
   tabSelect("bldg_'.$postVals[1].'", 1);
   </script>';
+  
+// get list of things that this building can be upgraded to
+
+$buildingInfo = explode('<-->', file_get_contents($gamePath.'/buildings.desc'));
+$bldgTypeInfo = explode(',', $buildingInfo[$postVals[1]*7+1]);
+
+for ($i=1; $i<sizeof($bldgTypeInfo); $i++) {
+	echo 'newBldgOpt("'.$i.'", "bldg_'.$postVals[1].'_tab3", "'.$buildingInfo[$i*7].'");';
+}
 
 ?>
