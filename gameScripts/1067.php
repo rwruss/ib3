c05ed366fcce821cec22f0f6a801d5cb2e8c3743
rwruss <rwruss81@gmail.com>
2016-04-20 16:32:50 -0500

    2016-04-20W3

--- gameScripts/1067.php
+++ gameScripts/1067.php
@@ -1,14 +1,20 @@
 <?php
 
 /*
-This will convert a small village or resource cart into a shared permanant city
+This will convert a resource cart into a permanant PLAYER sub-city
 */
+include('./cityClass.php');
+include("./slotFunctions.php");
 
 // Load the resource cart data (transported resources and buildings) and save to the players city slot (if not already linked)
+$unitFile = fopen($gamePath.'/unitDat.dat', 'r+b');
+fseek($unitFile, $_SESSION['selectedUnit']*$defaultblocksize);
+$unitDat = unpack('i*', fread($unitFile, 400));
 
-// Change all building statuses to undeployed
+// Change all building statuses to undeployed (should already been done via creation of the unit)
 
 // Create a new parent city ID and set parameters
+
 
 // Add the parent city ID to the current city
 
