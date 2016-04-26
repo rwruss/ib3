bdcab11c9cf061930b1145f5cad1e9daa329270e
rwruss <rwruss81@gmail.com>
2016-04-26 16:40:53 -0500

    2016-04-26W

--- public_html/dipFunctions.php
+++ public_html/dipFunctions.php
@@ -1,17 +1,37 @@
 <?php
 
 // Diplomacy functions
-function compareOrgs($unit1, $unit2, $slotFile) {
+function compareOrgs($player1, $player2, $unitFile) {
+	$tree_1 = parentTree($player1, $unitFile);
+	$tree_2 = parentTree($player2, $unitFile);
 	
+	$returnVal = false;
+	for ($i=0; $i<sizeof($tree_1); $i++) {
+		$match = array_search($tree_1[$i], $tree_2);
+		if ($match) {
+			$returnVal = tree_1[$i];
+			break;
+		} 
+	}
+	return $returnVal;
 }
 
 function parentTree($player, $unitFile) {
+	global $defaultBlockSize;
+	
+	$checkID = $player;
+	$parentList = [];
+	while ($checkID > 0) {
+		$parentList[] = $checkID;
+		fseek($unitFile, $checkID[1]*$defaultBlockSize);
+		$tmpDat = unpack('i*', fread($unitFile, 100));
 		
+		$checkID = $tmpDat[15];
+	}
+	
+	return $parentList;
 }
 
-function unitParentTree($unitID, $unitFile) {
-	
-}
 
 
 ?>
