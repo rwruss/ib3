46c497460956ae3f088631aa6e68badb47ff1952
rwruss <rwruss81@gmail.com>
2016-05-01 22:15:06 -0500

    2016-05-01H

--- gameScripts/1079.php
+++ gameScripts/1079.php
@@ -9,7 +9,7 @@ $unitFile = fopen($gamePath.'/unitDat.dat', 'rb');
 // Create a new plot and save the parameters
 $taskFile = fopen($gamePath.'/tasks.tdt', 'rb');
 $taskIndex = fopen($gamePath.'/tasks.tix', 'rb');
-$parameters = pack('i*', 0, 0,1,time(),0,0,2,$cityID,0, $cityID, $newID, $postVals[1], $_SESSION['selectedItem'], $pGameID, 1, 0);
+$parameters = pack('i*', 0, 0,1,time(),0,0,6,$_SESSION['selectedItem'],$pGameID,2);
 $newTask = createTask($taskFile, $taskIndex, 24*60, $parameters, $gamePath, $slotFile); //createTask($taskFile, $taskIndex, $duration, $parameters, $gamePath, $slotFile)
 fclose($taskFile);
 fclose($taskIndex);
