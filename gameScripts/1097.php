2c8804b9e1a707253e0c1bc730f3d708080bf0b7
rwruss <rwruss81@gmail.com>
2016-06-30 22:31:51 -0500

    2016-06-30H

--- gameScripts/1097.php
+++ gameScripts/1097.php
@@ -1,5 +1,15 @@
 <?php
 
 //Write a message to an object or city
+echo '<script>
+useDeskTop.newPane("characters");
+thisDiv = useDeskTop.getPane("characters");
+
+textBlob("", thisDiv, "To: '.$postVals[1].'");
+msgDiv = addDiv("", "", thisDiv);
+msgBox = document.createElement("textArea");
+msgDiv.appendChild(msgBox)';
+
+
 
 ?>
