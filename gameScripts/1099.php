085ed73d791b77c55356bc2a2f4936f4003c6750
rwruss <rwruss81@gmail.com>
2016-07-11 21:54:25 -0500

    2016-07-11H

--- gameScripts/1099.php
+++ gameScripts/1099.php
@@ -12,7 +12,17 @@ echo 'Check player '.$pGameID.' message slot '.$trgPlayer->unitDat[25];
 if ($trgPlayer->unitDat[25] == 0) {
   echo 'No messages';
 } else {
+  $msgFile = fopen($gamePath.'/messages.dat', 'rb');
   $msgSlot = new blockSlot($trgPlayer->unitDat[25], $slotFile, 40);
+  print_r($msgSlot->slotData);
+  for ($i=1; $i<=sizeof($msgSlot->slotData); $i+=3) {
+    if ($msgSlot->slotData[$i] > 0) {
+      fseek($msgFile, $msgSlot->slotData[$i]);
+      $msgLen = unpack('i', fread($msgFile, 4));
+      echo fread($msgFile, $msgLen[1]).'<br>';
+    }
+  }
+  fclose($msgFile);
 }
 fclose($unitFile);
 fclose($slotFile);
--- gameScripts/3001.php
+++ gameScripts/3001.php
@@ -18,6 +18,7 @@ $trgObject = new unit($msg[0], $unitFile, 400);
 print_r($trgObject->unitDat);
 
 //Determine who all to send it to based on target type
+$toList = [];
 switch($trgObject->unitDat[4]) {
   case 1: // a town object
     echo 'Send to all members of a town';
@@ -27,40 +28,47 @@ switch($trgObject->unitDat[4]) {
 
   case 10: // a tribe object
     echo 'This is a tribe... send to '.$trgObject->unitDat[6];
+    $toList[] = $trgObject->unitDat[6];
     break;
 }
-/*
-if ($trgPlayer->unitDat[25] == 0) {
-  if (flock($slotFile, LOCK_EX)) {
-    fseek($slotFile, 0, SEEK_END);
-    $use_slot = max(1, (ftell($slotFile))/40);
-    fseek($slotFile, $use_slot*40 +39);
-    fwrite($slotFile, pack("C", 0));
-    flock($slotFile, LOCK_UN); // release the lock
-    $trgPlayer->unitDat[25] = $use_slot;
+
+if (sizeof($toList) > 0) {
+  for ($i=0; $i<sizeof($toList); $i++) {
+    echo 'Record message for player '.$toList[$i];
+    $trgPlayer = new player($toList[$i], $unitFile, 400);
+    if ($trgPlayer->unitDat[25] == 0) {
+      if (flock($slotFile, LOCK_EX)) {
+        fseek($slotFile, 0, SEEK_END);
+        $use_slot = max(1, (ftell($slotFile))/40);
+        fseek($slotFile, $use_slot*40 +39);
+        fwrite($slotFile, pack("C", 0));
+        fflush($slotFile);
+        flock($slotFile, LOCK_UN); // release the lock
+        $trgPlayer->unitDat[25] = $use_slot;
+      }
+        echo 'Createa  new message slot at '.$trgPlayer->unitDat[25];
+    }
+    $msgSlot = new blockSlot($trgPlayer->unitDat[25], $slotFile, 40);
+
+    // Set unread flag
+    $trgPlayer->unitDat[5] = 1;
+    $trgPlayer->saveAll($unitFile);
+
+    // Record message contents in message file and message index
+    $messageContentFile = fopen($gamePath.'/messages.dat', 'r+b');
+    if (flock($messageContentFile, LOCK_EX)) {
+      fseek($messageContentFile, 0, SEEK_END);
+      $msgSpot = ftell($messageContentFile);
+      //fwrite($msg[1]);
+      $blockLength = strlen($msg[1]) + strlen($msg[2]) + 5 + 4;
+      echo 'Message length is '.$blockLength.' ('.strlen($msg[1]).') + ('.strlen($msg[2]).') + 9 written at spot '.$msgSpot.'<br>
+      Subject: '.$msg[1].'<br>
+      Content: '.$msg[2].'<br>';
+      fwrite($messageContentFile, pack('i', $blockLength).$msg[1].'<-!->'.$msg[2]);
+      $msgSlot->addItem($slotFile, pack('i*', $msgSpot, 1, 1), $msgSlot->findLoc(0, 3));  // message start loc, message file num, read/unread
+    }
   }
-    echo 'Createa  new message slot at '.$trgPlayer->unitDat[25];
 }
-$msgSlot = new blockSlot($trgPlayer->unitDat[25], $slotFile, 40);
-
-// Set unread flag
-$trgPlayer->unitDat[5] = 1;
-$trgPlayer->saveAll($unitFile);
-
-// Record message contents in message file and message index
-$messageContentFile = fopen($gamePath.'/messages.dat', 'r+b');
-if (flock($messageContentFile, LOCK_EX)) {
-  fseek($messageContentFile, 0, SEEK_END);
-  $msgSpot = ftell($messageContentFile);
-  //fwrite($msg[1]);
-  $blockLength = strlen($msg[1]) + strlen($msg[2]) + 5;
-  echo 'Message length is '.$blockLength.' ('.strlen($msg[1]).') + ('.strlen($msg[2]).') written at spot '.$msgSpot.'<br>
-  Subject: '.$msg[1].'<br>
-  Content: '.$msg[2].'<br>';
-
-  $msgSlot->addItem($slotFile, pack('i*', $msgSpot, 1, 1), $msgSlot->findLoc(0, 3));  // message start loc, message file num, read/unread
-}
-*/
 
 fclose($unitFile);
 fclose($slotFile);
